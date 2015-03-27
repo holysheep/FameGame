ad7d456035f11591cb7491d41c7809fc93a79ab4
holysheep <ardibest@gmail.com>
2015-03-27 16:14:31 +0300

    Task Load added

---
+++ app/src/main/java/gamecore/task/TaskLoadGamesPCInterface.java
@@ -0,0 +1,42 @@
+package gamecore.task;
+
+import android.os.AsyncTask;
+
+import com.android.volley.RequestQueue;
+
+import java.util.ArrayList;
+
+import gamecore.callbacks.PCgamesLoadedListener;
+import gamecore.extras.GameUtils;
+import gamecore.fragment.PCFragment;
+import gamecore.network.VolleySingleton;
+import gamecore.pojo.GameCat;
+import gamecore.services.TaskService;
+
+
+public class TaskLoadGamesPCInterface extends AsyncTask<Void, Void, ArrayList<GameCat>> {
+
+    private PCgamesLoadedListener myComponent;
+    private VolleySingleton volleySingleton;
+    private RequestQueue requestQueue;
+
+    public TaskLoadGamesPCInterface(PCgamesLoadedListener myComponent) {
+        this.myComponent = myComponent;
+        volleySingleton = VolleySingleton.getInstance();
+        requestQueue = volleySingleton.getRequestQueue();
+    }
+
+
+    @Override
+    protected ArrayList<GameCat> doInBackground(Void... params) {
+        ArrayList<GameCat> listGames = GameUtils.loadPCgames(requestQueue);
+        return listGames;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<GameCat> listGames) {
+        if (myComponent != null) {
+            myComponent.onPCgamesLoaded(listGames);
+        }
+    }
+}
