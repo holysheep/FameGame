998a4e42bfa854e05e441b15c0f72979dc3fc031
holysheep <ardibest@gmail.com>
2015-03-30 22:15:54 +0300

    Lru Bitmap Cache delete (not in use)

--- app/src/main/java/gamecore/network/VolleySingleton.java
+++ app/src/main/java/gamecore/network/VolleySingleton.java
@@ -21,42 +21,28 @@ public class VolleySingleton {
 
         mRequestQueue = Volley.newRequestQueue(MyApp.getAppContext());
         mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
-
             private LruCache<String, Bitmap> cache = new LruCache<>((int) (Runtime.getRuntime().maxMemory() / 1024) / 8);
-
             @Override
             public Bitmap getBitmap(String url) {
-
                 return cache.get(url);
             }
-
             @Override
             public void putBitmap(String url, Bitmap bitmap) {
-
                 cache.put(url, bitmap);
-
             }
         });
-
     }
 
     public static VolleySingleton getInstance() {
-
         if (sInstance == null) {
-
             sInstance = new VolleySingleton();
-
         }
-
         return sInstance;
-
     }
 
     public RequestQueue getRequestQueue() {
-
         return mRequestQueue;
     }
-
 
     public ImageLoader getmImageLoader() {
         getRequestQueue();
