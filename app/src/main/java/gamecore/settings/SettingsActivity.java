79323ea6c3d9c635048a3fcb5993d73f9c4ce48a
holysheep <ardibest@gmail.com>
2015-05-12 00:13:26 +0300

    Add preferences toolbar

--- app/src/main/java/gamecore/settings/SettingsActivity.java
+++ app/src/main/java/gamecore/settings/SettingsActivity.java
@@ -3,15 +3,24 @@ package gamecore.settings;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import gamecore.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.pref_with_toolbar);
 
+        toolbar = (Toolbar) findViewById(R.id.pref_toolbar);
+        setSupportActionBar(toolbar);
+        
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.content_frame, new SettingsFragment())
                 .commit();
     }
 }
